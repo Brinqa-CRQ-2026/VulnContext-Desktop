f2222124b78674a3f167fd3602be416a2c8469cd
mokimochiii <vimimoreno04@gmail.com>
2026-04-24 13:58:27 -0700

    updated secret key

--- backend/scripts/sync_kev.py
+++ backend/scripts/sync_kev.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
 
 KEV_URL = "https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json"
 
